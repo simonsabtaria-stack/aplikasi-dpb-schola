f22716caee1f59f96c2e475fc4ca0ca9d3ae6eb5
simonsabtaria-stack <simonsabtaria@gmail.com>
2026-06-02 19:29:28 +0700

    Update data_kurikulum.py

--- data_kurikulum.py
+++ data_kurikulum.py
@@ -1,32 +1,16 @@
 
 
+
 bank_kurikulum = {
-    "Pendidikan Agama Katolik": {
-        "Pribadi Peserta Didik": [
-            "Aku Citra Allah yang Unik", 
-            "Tugas Perkembanganku", 
-            "Kemampuanku dan Keterbatasanku"
-        ],
-        "Yesus Kristus": [
-            "Kisah Kelahiran Yesus", 
-            "Mukjizat-Mukjizat Yesus", 
-            "Perumpamaan tentang Kerajaan Allah"
-        ],
-        "Gereja": [
-            "Amanat Agung Yesus",
-            "Karya Pelayanan Gereja"
-        ]
-    },
-    "Ilmu Pengetahuan Alam (IPA)": {
-        "Pemahaman IPA": [
-            "Zat dan Perubahannya", 
-            "Sistem Tata Surya", 
-            "Ekologi dan Keanekaragaman Hayati"
-        ],
-        "Keterampilan Proses": [
-            "Mengamati dan Menanya", 
-            "Merencanakan Penyelidikan", 
-            "Menganalisis Data dan Bukti"
-        ]
-    }
+    "Pendidikan Agama Katolik dan Budi Pekerti": ["Pribadi Peserta Didik", "Yesus Kristus", "Gereja", "Masyarakat"],
+    "Pendidikan Pancasila": ["Pancasila", "Undang-Undang Dasar Negara RI Tahun 1945", "Bhinneka Tunggal Ika", "Negara Kesatuan Republik Indonesia"],
+    "Bahasa Indonesia": ["Menyimak", "Membaca dan Memirsa", "Berbicara dan Mempresentasikan", "Menulis"],
+    "Matematika": ["Bilangan", "Aljabar", "Pengukuran", "Geometri", "Analisis Data dan Peluang"],
+    "Ilmu Pengetahuan Alam (IPA)": ["Pemahaman IPA", "Keterampilan Proses"],
+    "Ilmu Pengetahuan Sosial (IPS)": ["Pemahaman IPS", "Keterampilan Proses"],
+    "Bahasa Inggris": ["Menyimak - Berbicara", "Membaca - Memirsa", "Menulis - Mempresentasikan"],
+    "Pend. Jasmani, Olahraga, dan Kesehatan (PJOK)": ["Keterampilan Gerak", "Pengetahuan Gerak", "Pemanfaatan Gerak", "Pengembangan Karakter"],
+    "Informatika": ["Berpikir Komputasional", "Teknologi Informasi dan Komunikasi", "Sistem Komputer", "Jaringan Komputer dan Internet", "Analisis Data", "Algoritma dan Pemrograman", "Dampak Sosial Informatika", "Praktik Lintas Bidang"],
+    "Seni (Musik/Rupa/Tari/Teater)": ["Mengalami (Experiencing)", "Menciptakan (Making/Creating)", "Merefleksikan (Reflecting)", "Berpikir dan Bekerja Artistik", "Berdampak (Impacting)"],
+    "Muatan Lokal": ["Ketik sendiri di file Word nanti...", "Lainnya..."]
 }
