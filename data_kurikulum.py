6bb016cf7162c05e36d2ed0491936adca9fd2c0b
simonsabtaria-stack <simonsabtaria@gmail.com>
2026-06-02 19:14:39 +0700

    Add files via upload

---
+++ data_kurikulum.py
@@ -0,0 +1,32 @@
+
+
+bank_kurikulum = {
+    "Pendidikan Agama Katolik": {
+        "Pribadi Peserta Didik": [
+            "Aku Citra Allah yang Unik", 
+            "Tugas Perkembanganku", 
+            "Kemampuanku dan Keterbatasanku"
+        ],
+        "Yesus Kristus": [
+            "Kisah Kelahiran Yesus", 
+            "Mukjizat-Mukjizat Yesus", 
+            "Perumpamaan tentang Kerajaan Allah"
+        ],
+        "Gereja": [
+            "Amanat Agung Yesus",
+            "Karya Pelayanan Gereja"
+        ]
+    },
+    "Ilmu Pengetahuan Alam (IPA)": {
+        "Pemahaman IPA": [
+            "Zat dan Perubahannya", 
+            "Sistem Tata Surya", 
+            "Ekologi dan Keanekaragaman Hayati"
+        ],
+        "Keterampilan Proses": [
+            "Mengamati dan Menanya", 
+            "Merencanakan Penyelidikan", 
+            "Menganalisis Data dan Bukti"
+        ]
+    }
+}
