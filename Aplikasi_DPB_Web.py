0dbd679982e14b4db00e70970095e679dcd17b46
simonsabtaria-stack <simonsabtaria@gmail.com>
2026-06-04 19:57:57 +0700

    Update Aplikasi_DPB_Web.py

--- Aplikasi_DPB_Web.py
+++ Aplikasi_DPB_Web.py
@@ -25,7 +25,7 @@ URL_DATABASE_SHEET = "https://script.google.com/macros/s/AKfycbyi9lnZJplhJDHV9Rk
 # ==========================================
 # 🔐 PENGATURAN BRANKAS SUPABASE (ISI DI SINI)
 # ==========================================
-SUPABASE_URL = "https://yblxuksszfubyfpnywwg.supabase.co/rest/v1/"
+SUPABASE_URL = "https://yblxuksszfubyfpnywwg.supabase.co"
 SUPABASE_KEY = "sb_publishable_lZgTirSMV0P3QwFtKMjc1Q_wP-SWroz"
 
 # Inisialisasi Koneksi Supabase
