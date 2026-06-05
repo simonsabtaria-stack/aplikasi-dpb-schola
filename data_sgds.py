4b7573de120f3f156877b2fb35810e95342bf6b9
simonsabtaria-stack <simonsabtaria@gmail.com>
2026-06-05 22:26:27 +0700

    Add files via upload

---
+++ data_sgds.py
@@ -0,0 +1,22 @@
+# File: data_sdgs.py
+# Database TP SDGs berdasarkan dokumen UNESCO Pendidikan untuk Tujuan Pembangunan Berkelanjutan
+
+bank_sdgs = {
+    "1. Tanpa Kemiskinan": "Mengakhiri segala bentuk kemiskinan di mana pun.",
+    "2. Tanpa Kelaparan": "Mengakhiri kelaparan, mencapai ketahanan pangan dan memperbaiki gizi dan mendorong pertanian berkelanjutan.",
+    "3. Kehidupan Sehat dan Sejahtera": "Menjamin kehidupan yang sehat dan meningkatkan kesejahteraan bagi semua orang di segala usia.",
+    "4. Pendidikan Berkualitas": "Menjamin pendidikan berkualitas yang inklusif dan adil serta mendorong kesempatan belajar seumur hidup bagi semua.",
+    "5. Kesetaraan Gender": "Mencapai kesetaraan gender dan memberdayakan seluruh perempuan dan anak perempuan.",
+    "6. Air Bersih dan Sanitasi Layak": "Menjamin ketersediaan dan pengelolaan air dan sanitasi berkelanjutan untuk semua.",
+    "7. Energi Bersih dan Terjangkau": "Pastikan akses terhadap energi yang terjangkau, andal, berkelanjutan, dan bersih untuk semua.",
+    "8. Pekerjaan Layak dan Pertumbuhan Ekonomi": "Mendorong pertumbuhan ekonomi yang berkelanjutan, inklusif dan berkelanjutan, lapangan kerja penuh dan produktif serta pekerjaan yang layak untuk semua.",
+    "9. Industri, Inovasi dan Infrastruktur": "Membangun infrastruktur yang berketahanan, mendorong industrialisasi yang inklusif dan berkelanjutan, serta mendorong inovasi.",
+    "10. Berkurangnya Kesenjangan": "Mengurangi ketimpangan di dalam dan di luar wilayah antar negara.",
+    "11. Kota dan Permukiman yang Berkelanjutan": "Membangun kota dan pemukiman manusia yang inklusif, aman, berketahanan dan berkelanjutan.",
+    "12. Konsumsi dan Produksi yang Bertanggung Jawab": "Memastikan pola konsumsi dan produksi yang berkelanjutan.",
+    "13. Penanganan Perubahan Iklim": "Mengambil tindakan segera untuk memerangi perubahan iklim dan dampaknya.",
+    "14. Ekosistem Lautan": "Melestarikan dan memanfaatkan sumber daya samudera, lautan, dan kelautan secara berkelanjutan untuk pembangunan berkelanjutan.",
+    "15. Ekosistem Daratan": "Melindungi, memulihkan dan mendorong pemanfaatan ekosistem darat secara berkelanjutan, mengelola hutan secara berkelanjutan, memerangi penggurunan, dan menghentikan dan membalikkan degradasi lahan serta menghentikan hilangnya keanekaragaman hayati.",
+    "16. Perdamaian, Keadilan dan Kelembagaan yang Tangguh": "Mempromosikan masyarakat yang damai dan inklusif untuk pembangunan berkelanjutan, memberikan akses terhadap keadilan bagi semua dan membangun Institusi yang efektif, akuntabel dan inklusif di semua tingkatan.",
+    "17. Kemitraan untuk Mencapai Tujuan": "Memperkuat sarana implementasi dan merevitalisasi kemitraan global untuk pembangunan berkelanjutan."
+}
