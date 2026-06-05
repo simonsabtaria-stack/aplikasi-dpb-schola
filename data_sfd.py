d0bdee4bd6e32aebfc5cab9777a84d8aec13358f
simonsabtaria-stack <simonsabtaria@gmail.com>
2026-06-05 20:19:32 +0700

    Add files via upload

--- data_sfd.py
+++ data_sfd.py
@@ -1,61 +1,181 @@
 # File: data_sfd.py
-# Database Core Values / Nilai Ke-SFD-an (Versi Ramping)
+# Database Core Values / Nilai Ke-SFD-an untuk Aplikasi DPB Amoris
 
 bank_sfd = {
     "SEMANGAT": {
         "CINTA KASIH": {
-            "TK": "Menceritakan (C2) contoh perbuatan mengangkat martabat seluruh ciptaan, Membantu (A2) anak untuk berani memaafkan, dan Melakukan (P2) praktik membagikan diri.",
-            "SD": "Melatih (C4) mengenai membagi diri, dengan Ikut serta (A3) dan Mengembangkan (P4) diri untuk berani memaafkan.",
-            "SMP": "Mengarahkan (C5) untuk membagikan diri, dan Mengubah perilaku (A5) memaafkan yang didorong dari dalam diri, serta Menentukan (P5) tindakan untuk mengangkat martabat seluruh ciptaan."
+            "Mengangkat martabat seluruh ciptaan": {
+                "TK": "Menceritakan (C2), Membantu (A2), Melakukan (P2)",
+                "SD": "Mendifferensiasikan (C4), Ikut serta (A3), Mengendalikan (P3)",
+                "SMP": "Memberi saran (C5), Bertindak (A5), Mengelola (P5)"
+            },
+            "Membagikan diri": {
+                "TK": "SKL: Menerangkan (C2) mengenai mengangkat martabat seluruh ciptaan dengan Melaksanakan (A2) dorongan dari hati untuk berani memaafkan dan Mempraktikkan (P2) membagi diri dalam tindakan terhadap sesama.",
+                "SD": "SKL: Mendifferensiasikan (C4) mengenai membagi diri, dengan Ikut serta (A3) mengangkat martabat seluruh ciptaan dan Mengendalikan (P3) diri untuk berani memaafkan.",
+                "SMP": "SKL: Memberi saran (C5) untuk membagikan diri, dan Bertindak (A5) memaafkan yang didorong dari dalam diri, serta Mengelola (P5) tindakan untuk mengangkat martabat seluruh ciptaan."
+            },
+            "Berani memaafkan": {
+                "TK": "CN: Menceritakan (C2) contoh perbuatan mengangkat martabat seluruh ciptaan, Membantu (A2) anak untuk berani memaafkan, dan Melakukan (P2) praktik membagikan diri.",
+                "SD": "CN: Melatih (C4) mengenai membagi diri, dengan Ikut serta (A3) dan Mengembangkan (P4) diri untuk berani memaafkan.",
+                "SMP": "CN: Mengarahkan (C5) untuk membagikan diri, dan Mengubah perilaku (A5) memaafkan yang didorong dari dalam diri, serta Menentukan (P5) tindakan untuk mengangkat martabat seluruh ciptaan."
+            }
         },
         "KESEDERHANAAN KRISTIANI": {
-            "TK": "Menjelaskan (C2) tentang hidup ugahari dengan Menampilkan (A2) hidup yang layak dipercaya dan Mempersiapkan (P2) anak menjadi teladan.",
-            "SD": "Melatih (C4) hidup ugahari dan Memadukan pendapat (A4) untuk layak dipercaya serta Mengelola (P5) diri menjadi teladan.",
-            "SMP": "Menyiapkan (C6) menjadi teladan dengan Pembiasaan (A5) diri layak dipercaya serta Mengelola (P5) hidup ugahari."
+            "Layak Dipercaya (Jujur/tidak mendua hati)": {
+                "TK": "Mengingat kembali (C1), Menyatakan setuju (A2), Melaksanakan (P2)",
+                "SD": "Menemukan (C3), Menyatakan pendapat (A3), Mengembangkan (P4)",
+                "SMP": "Memutuskan (C6), Mempertahankan (A5), Menentukan (P5)"
+            },
+            "Hidup ugahari": {
+                "TK": "SKL: Mengingat kembali (C1) hidup ugahari dan Menyatakan setuju (A2) untuk layak dipercaya dengan Melaksanakan (P2) tindakan menjadi teladan.",
+                "SD": "SKL: Menemukan (C3) hidup ugahari dan Menyatakan pendapat (A3) untuk Layak Dipercaya serta Mengembangkan (P4) diri menjadi teladan.",
+                "SMP": "SKL: Memutuskan (C6) menjadi teladan dengan Mempertahankan (A5) diri layak dipercaya serta Menentukan (P5) hidup ugahari."
+            },
+            "Menjadi teladan": {
+                "TK": "CN: Menjelaskan (C2) tentang hidup ugahari dengan Menampilkan (A2) hidup yang layak dipercaya dan Mempersiapkan (P2) anak menjadi teladan.",
+                "SD": "CN: Melatih (C4) hidup ugahari dan Memadukan pendapat (A4) untuk layak dipercaya serta Mengelola (P5) diri menjadi teladan.",
+                "SMP": "CN: Menyiapkan (C6) menjadi teladan dengan Pembiasaan (A5) diri layak dipercaya serta Mengelola (P5) hidup ugahari."
+            }
         },
         "RAJIN DAN GIAT": {
-            "TK": "Menunjukkan sikap Kontemplatif Aktif dan Ikut serta (A3) untuk tulus hati serta Menunjukkan (P3) tindakan tekun dan setia.",
-            "SD": "Menguraikan (C4) ketekunan dan kesetiaan dan Membangun (A4) hidup seimbang yang didorong dari dalam diri serta Mengembangkan (P4) ketulusan dalam bertindak.",
-            "SMP": "Membuktikan (C5) ketulusan hati dengan Membangun (A4) hidup seimbang dan Mengelola (P5) ketekunan dan kesetiaan dalam kehidupan setiap saat."
+            "Tulus hati": {
+                "TK": "Menceritakan (C2), Berlatih (A2), Mempertunjukkan (P2)",
+                "SD": "Menguraikan (C4), Menunjukkan (A3), Mempraktekkan (P3)",
+                "SMP": "Mengevaluasi (C5), Berpegang pada (A4), Melakukan (P5)"
+            },
+            "Hidup seimbang (vita mixta-Kontemplatif Aktif)": {
+                "TK": "SKL: Menceritakan (C2) hidup seimbang dan Berlatih (A2) untuk tulus hati serta Mempertunjukkan (P2) tindakan tekun dan setia.",
+                "SD": "SKL: Menguraikan (C4) ketekunan dan kesetiaan dan Menunjukkan (A3) hidup seimbang yang didorong dari dalam diri serta Mempraktekkan (P5) ketulusan dalam bertindak.",
+                "SMP": "SKL: Mengevaluasi (C5) ketulusan hati dengan Berpegang pada (A4) hidup seimbang dan Melakukan (P5) ketekunan dan kesetiaan dalam kehidupan setiap saat."
+            },
+            "Tekun dan Setia": {
+                "TK": "CN: Menunjukkan sikap Kontemplatif Aktif dan Ikut serta (A3) untuk tulus hati serta Menunjukkan (P3) tindakan tekun dan setia.",
+                "SD": "CN: Menguraikan (C4) ketekunan dan kesetiaan dan Membangun (A4) hidup seimbang yang didorong dari dalam diri serta Mengembangkan (P4) ketulusan dalam bertindak.",
+                "SMP": "CN: Membuktikan (C5) ketulusan hati dengan Membangun (A4) hidup seimbang dan Mengelola (P5) ketekunan dan kesetiaan dalam kehidupan setiap saat."
+            }
         },
         "LEPAS BEBAS": {
-            "TK": "Mengartikan (C2) ketaatan dengan Mengajak (A3) penyerahan diri total kepada Allah dan Melaksanakan (P2) tindakan dengan murah hati.",
-            "SD": "Menguraikan (C4) ketaatan dengan Menekankan (A3) dalam penyerahan diri total di hadapan Allah serta Mengembangkan (P4) tindakan dengan murah hati.",
-            "SMP": "Mengarahkan (C5) ketaatan, dan Membiasakan (A5) penyerahan diri secara total kepada Allah serta Mengintegrasikan (P4) aksi murah hati dalam berbuat dan bertindak."
+            "Taat": {
+                "TK": "Menceritakan (C2), Mempraktikkan (A2), Beraksi (P2)",
+                "SD": "Menguraikan (C4), Ikut serta (A3), Mempraktekkan (P3)",
+                "SMP": "Mengevaluasi (C5), Mempertahankan (A5), Mengembangkan (P4)"
+            },
+            "Penyerahan diri total kepada Allah (Providentia Dei)": {
+                "TK": "SKL: Menceritakan (C2) ketaatan, dengan Mempraktekkan (A2) penyerahan diri secara total kepada Allah dan Beraksi (P2) dalam tindakan dengan murah hati.",
+                "SD": "SKL: Menguraikan (C4) ketaatan, dengan Ikut serta (A3) dalam penyerahan diri total di hadapan Allah serta Mempraktekkan (P3) tindakan dengan murah hati.",
+                "SMP": "SKL: Mengevaluasi (C5) ketaatan, dan Mempertahankan (A5) penyerahan diri secara total kepada Allah serta Mengembangkan (P4) aksi murah hati dalam berbuat dan bertindak."
+            },
+            "Murah hati": {
+                "TK": "CN: Mengartikan (C2) ketaatan dengan Mengajak (A3) penyerahan diri total kepada Allah dan Melaksanakan (P2) tindakan dengan murah hati.",
+                "SD": "CN: Menguraikan (C4) ketaatan dengan Menekankan (A3) dalam penyerahan diri total di hadapan Allah serta Mengembangkan (P4) tindakan dengan murah hati.",
+                "SMP": "CN: Mengarahkan (C5) ketaatan, dan Membiasakan (A5) penyerahan diri secara total kepada Allah serta Mengintegrasikan (P4) aksi murah hati dalam berbuat dan bertindak."
+            }
         },
         "SEMANGAT DOA": {
-            "TK": "Mendemonstrasikan (C3) dengan mewujudkan doa dalam tindakan nyata dan Memilih (A3) berdiskresi dengan Mempraktekkan (P3) tapa brata ke dalam hidup.",
-            "SD": "Memaksimalkan (C4) tapa brata, serta Memprakarsai (A3) berdiskresi dengan Mengembangkan (P4) perwujudan doa dalam tindakan nyata.",
-            "SMP": "Membuktikan (C5) Perwujudan doa dalam tindakan nyata, serta Memadukan (A4) Diskresi dan Mengelola (P5) Tapa brata dalam seluruh tindakan."
+            "Tapa brata": {
+                "TK": "Mengartikan (C2), Menampilkan (A2), Bereaksi (P2)",
+                "SD": "Menganalisis (C4), Mengajak (A3), Mempraktekkan (P3)",
+                "SMP": "Mendukung (C5), Berpegang pada (A4), Melaksanakan (P5)"
+            },
+            "Berdiskresi (Pembedaan Roh)": {
+                "TK": "SKL: Mengartikan (C2) tapa brata dengan Menampilkan (A2) diskresi serta Bereaksi (P2) dalam perwujudan doa melalui tindakan nyata.",
+                "SD": "SKL: Menganalisis (C4) tapa brata, serta Mengajak (A3) berdiskresi dengan Mempraktekkan (P3) perwujudan doa dalam tindakan nyata.",
+                "SMP": "SKL: Mendukung (C5) Perwujudan doa dalam tindakan nyata, serta Berpegang pada (A4) Diskresi dan Melaksanakan (P5) Tapa brata dalam seluruh tindakan."
+            },
+            "Mewujudkan doa dalam tindakan nyata": {
+                "TK": "CN: Mendemonstrasikan (C3) dengan mewujudkan doa dalam tindakan nyata dan Memilih (A3) berdiskresi dengan Mempraktekkan (P3) tapa brata ke dalam hidup.",
+                "SD": "CN: Memaksimalkan (C4) tapa brata, serta Memprakarsai (A3) berdiskresi dengan Mengembangkan (P4) perwujudan doa dalam tindakan nyata.",
+                "SMP": "CN: Membuktikan (C5) Perwujudan doa dalam tindakan nyata, serta Memadukan (A4) Diskresi dan Mengelola (P5) Tapa brata dalam seluruh tindakan."
+            }
         }
     },
     "FRATERNITAS": {
         "MENJADI HAMBA": {
-            "TK": "Menjelaskan (C3) tindakan membawa damai, serta Mengajak (A3) untuk melayani dengan penuh cinta, serta Membangun (P2) sikap berpihak pada yang lemah.",
-            "SD": "Menyimpulkan (C4) keberpihakan kepada yang lemah, Memposisikan (A3) diri melayani dengan cinta, Membangun (P4) sikap agen pembawa damai.",
-            "SMP": "Mengarahkan (C5) untuk membawa damai, serta Memadukan (A4) rasa keberpihakan pada yang lemah, dengan Menggabungkan (P4) pelayanan."
+            "Membawa damai, menjadi ragi keadilan, dan kasih": {
+                "TK": "Menceritakan (C2), Melakukan (A2), Menirukan (P1)",
+                "SD": "Menelaah (C4), Ikut serta (A3), Mempraktekkan (P3)",
+                "SMP": "Memberi saran (C5), Mengkoordinir (A4), Mengintegrasikan (P4)"
+            },
+            "Melayani dengan penuh cinta dan perhatian": {
+                "TK": "SKL: Menceritakan (C2) Keberpihakan pada yang lemah dengan Melakukan (A2) dorongan membawa damai, serta Menirukan (P1) aksi pelayanan.",
+                "SD": "SKL: Menelaah (C4) Keberpihakan pada yang lemah dengan Ikut serta (A3) melayani penuh cinta dalam Mempraktekkan (P3) membawa damai.",
+                "SMP": "SKL: Memberi saran (C5) untuk membawa damai, serta Mengkoordinir (A4) rasa keberpihakan pada yang lemah, dengan Mengintegrasikan (P4) pelayanan."
+            },
+            "Berpihak pada yang lemah, miskin, tersingkir": {
+                "TK": "CN: Menjelaskan (C3) tindakan membawa damai, serta Mengajak (A3) untuk melayani dengan penuh cinta, serta Membangun (P2) sikap berpihak pada yang lemah.",
+                "SD": "CN: Menyimpulkan (C4) keberpihakan kepada yang lemah, Memposisikan (A3) diri melayani dengan cinta, Membangun (P4) sikap agen pembawa damai.",
+                "SMP": "CN: Mengarahkan (C5) untuk membawa damai, serta Memadukan (A4) rasa keberpihakan pada yang lemah, dengan Menggabungkan (P4) pelayanan."
+            }
         },
         "BERJALAN BERSAMA": {
-            "TK": "Menerangkan sikap berbelarasa dengan penuh ketulusan (C2) dengan Mendiskusikan untuk bersatu hati (A2) serta Melaksanakan (P3) memelihara hidup ciptaan.",
-            "SD": "Membuktikan (C5) pemeliharaan semua ciptaan dengan Membangun serta (A4) bersatu hati dalam Mengembangkan (P4) belarasa.",
-            "SMP": "Merancang (C6) cara memelihara semua ciptaan, dengan Membentuk pendapat (A4) Bersatu hati serta Membangun (P4) belarasa."
+            "Bersatu hati (Peka terhadap diri, sesama, zaman)": {
+                "TK": "Mengartikan (C2), Berlatih (A2), Melaksanakan (P2)",
+                "SD": "Membuktikan (C5), Ikut serta (A3), Mempraktekkan (P3)",
+                "SMP": "Membuat pola (C6), Berpegang pada (A4), Mengembangkan (P4)"
+            },
+            "Berbelarasa dengan penuh ketulusan": {
+                "TK": "SKL: Mengartikan (C2) belarasa dengan penuh ketulusan, dan Berlatih (A2) untuk bersatu hati serta Melaksanakan (P2) pemeliharaan hidup.",
+                "SD": "SKL: Membuktikan (C5) pemeliharaan semua ciptaan dengan Ikut serta (A3) bersatu hati dalam Mempraktekkan (P3) belarasa.",
+                "SMP": "SKL: Membuat pola (C6) cara memelihara semua ciptaan, dengan Berpegang pada (A4) Bersatu hati serta Mengembangkan (P4) belarasa."
+            },
+            "Memelihara hidup semua ciptaan": {
+                "TK": "CN: Menerangkan sikap berbelarasa dengan penuh ketulusan (C2) dengan Mendiskusikan untuk bersatu hati (A2) serta Melaksanakan (P3) memelihara hidup ciptaan.",
+                "SD": "CN: Membuktikan (C5) pemeliharaan semua ciptaan dengan Membangun serta (A4) bersatu hati dalam Mengembangkan (P4) belarasa.",
+                "SMP": "CN: Merancang (C6) cara memelihara semua ciptaan, dengan Membentuk pendapat (A4) Bersatu hati serta Membangun (P4) belarasa."
+            }
         }
     },
     "DINA": {
         "KERENDAHAN HATI / KENOSIS": {
-            "TK": "Menjelaskan (C2) tentang penyangkalan diri dengan Mempraktekan (A3) sikap rela berkorban untuk Menunjukkan (P3) sikap perendahan diri.",
-            "SD": "Memilih (C5) rela berkorban dengan Membiasakan (A5) kesadaran perendahan diri dan Mempraktekkan (P3) penyangkalan diri.",
-            "SMP": "Menampilkan (C6) untuk rela berkorban dan Mengubah perilaku (A5) dengan tulus melalui penyangkalan diri serta Menentukan (P5) tindakan perendahan diri."
+            "Perendahan diri (Kemiskinan Roh)": {
+                "TK": "Menyebutkan (C1), Menawarkan (A2), Menerapkan (P2)",
+                "SD": "Menguraikan (C4), Menyempurnakan (A3), Mempraktekkan (P3)",
+                "SMP": "Memutuskan (C6), Melayani (A5), Mengelola (P5)"
+            },
+            "Penyangkalan diri": {
+                "TK": "SKL: Menyebutkan (C1) penyangkalan diri dan Menawarkan (A2) perendahan diri serta Menerapkan (P2) kerelaan berkorban.",
+                "SD": "SKL: Menguraikan (C4) rela berkorban dengan Menyempurnakan (A3) kesadaran perendahan diri dan Mempraktekkan (P3) penyangkalan diri.",
+                "SMP": "SKL: Memutuskan (C6) untuk rela berkorban dan Melayani (A5) dengan tulus melalui penyangkalan diri serta Mengelola (P5) tindakan perendahan diri."
+            },
+            "Rela berkorban": {
+                "TK": "CN: Menjelaskan (C2) tentang penyangkalan diri dengan Mempraktekan (A3) sikap rela berkorban untuk Menunjukkan (P3) sikap perendahan diri.",
+                "SD": "CN: Memilih (C5) rela berkorban dengan Membiasakan (A5) kesadaran perendahan diri dan Mempraktekkan (P3) penyangkalan diri.",
+                "SMP": "CN: Menampilkan (C6) untuk rela berkorban dan Mengubah perilaku (A5) dengan tulus melalui penyangkalan diri serta Menentukan (P5) tindakan perendahan diri."
+            }
         },
         "PERTOBATAN TERUS-MENERUS": {
-            "TK": "Menjelaskan (C2) arti matiraga dengan Mempraktekan (A2) cara menyesali dosa sehingga Mempersiapkan (P2) anak menjadi agen pembaharu.",
-            "SD": "Mengetes (C5) matiraga dengan Membiasakan (C5) penyesalan dosa serta Mengembangkannya (P4) dalam tindakan menjadi agen pembaharu.",
-            "SMP": "Membangun (C6) mati raga dan Membuktikan (A5) penyesalan dosa serta Mengelola (P5) aksi menjadi agen pembawa damai."
+            "Menyesali dosa dan kesalahan": {
+                "TK": "Memberi contoh (C2), Mempraktekkan (A2), Bereaksi (P2)",
+                "SD": "Mengevaluasi (C5), Menunjukkan (A3), Menyempurnakannya (P3)",
+                "SMP": "Membuat pola (C6), Menyatakan (A5), Membangun (P5)"
+            },
+            "Matiraga": {
+                "TK": "SKL: Memberi contoh (C2) matiraga dengan Mempraktekkan (A2) penyesalan dosa dan Bereaksi (P2) menjadi pembawa damai.",
+                "SD": "SKL: Mengevaluasi (C5) matiraga dengan Menunjukkan (A3) penyesalan dosa serta Menyempurnakannya (P3) dalam tindakan menjadi agen pembaharu.",
+                "SMP": "SKL: Membuat pola (C6) mati raga dan Menyatakan (A5) penyesalan dosa serta Membangun (P5) aksi menjadi agen pembawa damai."
+            },
+            "Menjadi Agen Pembaharu": {
+                "TK": "CN: Menjelaskan (C2) arti matiraga dengan Mempraktekan (A2) cara menyesali dosa sehingga Mempersiapkan (P2) anak menjadi agen pembaharu.",
+                "SD": "CN: Mengetes (C5) matiraga dengan Membiasakan (C5) penyesalan dosa serta Mengembangkannya (P4) dalam tindakan menjadi agen pembaharu.",
+                "SMP": "CN: Membangun (C6) mati raga dan Membuktikan (A5) penyesalan dosa serta Mengelola (P5) aksi menjadi agen pembawa damai."
+            }
         },
         "BERSUKACITA": {
-            "TK": "Menampilkan (C2) sikap bersahaja memilih (A3) rela menanggung derita dengan Mempersiapkan (P2) anak untuk tetap bahagia menjadi yang paling dina.",
-            "SD": "Mempertahankan pendapat (C5) kerelaan menanggung derita, dan Meyakinkan (A3) bahagia menjadi yang paling Dina serta Mempraktekkan (P3) bersahaja.",
-            "SMP": "Menghubungkan (C6) kerelaan menanggung derita dengan Menata (A4) diri untuk bersahaja serta Mengelola (P5) kebiasaan diri merasa bahagia menjadi yang paling Dina."
+            "Rela menanggung derita": {
+                "TK": "Mengartikan (C1), Menyatakan setuju (A2), Menerapkan (P2)",
+                "SD": "Menerima pendapat (C4), Membenarkan (A3), Mempraktekkan (P3)",
+                "SMP": "Memutuskan (C6), Mengatur (A4), Melakukan (P5)"
+            },
+            "Bersahaja dengan penuh kesederhanaan": {
+                "TK": "SKL: Mengartikan (C1) bersahaja dan Menyatakan setuju (A2) dari hati bahwa bahagia menjadi yang paling Dina, serta Menerapkan (P2) rela menanggung derita.",
+                "SD": "SKL: Menerima pendapat (C4) kerelaan menanggung derita, dan Membenarkan (A3) bahagia menjadi yang paling Dina, serta Mempraktekkan (P3) bersahaja.",
+                "SMP": "SKL: Memutuskan (C6) kerelaan menanggung derita dengan Mengatur (A4) diri untuk bersahaja serta Melakukan (P5) kebiasaan bahagia menjadi yang paling Dina."
+            },
+            "Bahagia menjadi yang paling dina": {
+                "TK": "CN: Menampilkan (C2) sikap bersahaja memilih (A3) rela menanggung derita dengan Mempersiapkan (P2) anak untuk tetap bahagia menjadi yang paling dina.",
+                "SD": "CN: Mempertahankan pendapat (C5) kerelaan menanggung derita, dan Meyakinkan (A3) bahagia menjadi yang paling Dina serta Mempraktekkan (P3) bersahaja.",
+                "SMP": "CN: Menghubungkan (C6) kerelaan menanggung derita dengan Menata (A4) diri untuk bersahaja serta Mengelola (P5) kebiasaan diri merasa bahagia menjadi yang paling Dina."
+            }
         }
     }
 }
